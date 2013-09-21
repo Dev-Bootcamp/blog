b9567d1579f6c1508b3f04fc5544f6b7a21f7059
Geoff Crittenden <geoffcritt@hotmail.com>
2013-09-21 10:06:51 -0500

    added feature edit posts

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -16,6 +16,16 @@ get '/create_post' do
   erb :create_post
 end
 
+get '/edit/:id' do
+  @post = Post.find(params[:id])
+  tags = []
+  @post.tags.each { |tag| tags << tag.name }
+  @tags = tags.join(', ')
+  p @tags
+  p @tags.class
+  erb :edit
+end
+
 # ==================================
 
 post '/create_post' do
@@ -23,6 +33,14 @@ post '/create_post' do
   @post = Post.new(params[:post])
   @post.save
   @tags = (params[:tag][:names].split(',')).collect { |tag| tag.strip }
+  @tags.each { |tag| @post.tags << Tag.find_or_create_by_name(tag) }
+  redirect to ("/posts/#{@post.id}")
+end
+
+post '/edit_post/:id' do
+  @post = Post.update(params[:id],params[:post])
+  @tags = (params[:tag][:names].split(',')).collect { |tag| tag.strip }
+  @post = Post.update(params[:id], :tags => [] )
   @tags.each { |tag| @post.tags << Tag.find_or_create_by_name(tag) }
   redirect to ("/posts/#{@post.id}")
 end
